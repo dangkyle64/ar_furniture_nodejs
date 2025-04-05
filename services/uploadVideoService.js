fcf939b31732a8d3fc313d980ad16dab3e1beba0
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-05 07:17:41 -0700

    basic implementation of frame splitting using the opencv library from video
    author: dangkyle64
    date: 4/5/2025

--- services/uploadVideoService.js
+++ services/uploadVideoService.js
@@ -1,8 +1,35 @@
+import fs from 'fs';
+import cv from 'opencv4nodejs-prebuilt-install';
+
 export const processVideo = async (videoBuffer) => {
-    console.log('Video received with size:', videoBuffer.length, 'butes');
+    const tempVideoPath = './temp_video.mp4';
+    
+    fs.writeFileSync(tempVideoPath, videoBuffer);
+
+    const videoCaptureObj = new cv.VideoCapture(tempVideoPath);
+
+    let frame;
+    let frameCount = 0;
+    let frames = [];
+
+    while(true) {
+        frame  = videoCaptureObj.read();
+
+        if (frame.empty) {
+            break;
+        };
+        frameCount++;
+    };
+
+
+    videoCaptureObj.release();
+    cv.destroyAllWindows();
+
+    fs.unlinkSync(tempVideoPath.tempVideoPath);
 
     return {
         message: 'Video processed successfully in-memory',
-        videoSize: videoBuffer.length,
+        videoSize: frames.length,
+        frameCount: frameCount
     };
 };
