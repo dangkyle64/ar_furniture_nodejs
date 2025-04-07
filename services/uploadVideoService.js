19b0b23ba30d71be2350d2e52dcefcfb89d270a3
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-07 10:16:22 -0700

    required to switch from .webm format from mediaRecorder object to .mp4 for the openCV library, install fluent-ffmpeg to do the webm to mp4 conversion, add helper function to do the ffmpeg conversion, required to use promise since ffmpeg is event driven returning .on and on error
    author: dangkyle64
    date: 4/7/2025

--- services/uploadVideoService.js
+++ services/uploadVideoService.js
@@ -1,35 +1,60 @@
 import fs from 'fs';
 import cv from 'opencv4nodejs-prebuilt-install';
+import ffmpeg from 'fluent-ffmpeg';
 
 export const processVideo = async (videoBuffer) => {
     const tempVideoPath = './recorded-video.webm';
-    
+    const tempMp4Path = './temp-vide-.mp4';
+
     fs.writeFileSync(tempVideoPath, videoBuffer);
 
-    const videoCaptureObj = new cv.VideoCapture(tempVideoPath);
+    try {
+        await convertWebmToMp4(tempVideoPath, tempMp4Path);
 
-    let frame;
-    let frameCount = 0;
-    let frames = [];
+        const videoCaptureObj = new cv.VideoCapture(tempMp4Path);
 
-    while(true) {
-        frame  = videoCaptureObj.read();
+        let frame;
+        let frameCount = 0;
+        let frames = [];
 
-        if (frame.empty) {
-            break;
+        while(true) {
+            frame  = videoCaptureObj.read();
+
+            if (frame.empty) {
+                break;
+            };
+            frameCount++;
         };
-        frameCount++;
+
+        videoCaptureObj.release();
+        cv.destroyAllWindows();
+        fs.unlinkSync(tempVideoPath); 
+        fs.unlinkSync(tempMp4Path);   
+
+        console.log({
+            message: 'Video processed successfully in-memory',
+            frameCount: frameCount
+        });
+        
+    } catch(error) {
+        console.error('Error processing video:', err);
     };
+};
 
+const convertWebmToMp4 = (inputPath, outputPath) => {
+    return new Promise((resolve, reject) => {
+        ffmpeg(inputPath)
+            .output(outputPath)
+            .on('end', () => resolve())
+            .on('error', (err) => reject('Error converting video: ' + err))
+            .run();
+    });
+};
 
-    videoCaptureObj.release();
-    cv.destroyAllWindows();
-
-    fs.unlinkSync(tempVideoPath.tempVideoPath);
-
-    return {
+/**
+ * return {
         message: 'Video processed successfully in-memory',
         videoSize: frames.length,
         frameCount: frameCount
     };
-};
+ */
